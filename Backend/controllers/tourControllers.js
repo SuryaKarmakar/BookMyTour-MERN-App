1d48d88ace86f16fbdfa4f1560de1c2b4799e209
surya-codeclouds <surya.karmakar@codeclouds.co.in>
2024-07-23 17:24:01 +0530

    controllers funcation added

---
+++ Backend/controllers/tourControllers.js
@@ -0,0 +1,9 @@
+exports.getAllTours = (req, res) => {};
+
+exports.getSingleTour = (req, res) => {};
+
+exports.createNewTour = (req, res) => {};
+
+exports.updateTour = (req, res) => {};
+
+exports.deleteTour = (req, res) => {};
---
+++ Backend/controllers/userControllers.js
@@ -0,0 +1,9 @@
+exports.getAllUsers = (req, res) => {};
+
+exports.createNewUser = (req, res) => {};
+
+exports.getSingleUser = (req, res) => {};
+
+exports.updateUser = (req, res) => {};
+
+exports.deleteUser = (req, res) => {};
