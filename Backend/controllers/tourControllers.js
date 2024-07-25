79fab86b6c11019cc072b52c5842a6aec2f9b7c1
surya-codeclouds <surya.karmakar@codeclouds.co.in>
2024-07-25 19:04:55 +0530

    tour and user controller added

--- Backend/controllers/tourControllers.js
+++ Backend/controllers/tourControllers.js
@@ -1,9 +1,39 @@
-exports.getAllTours = (req, res) => {};
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "tours get successfully",
+    data: {},
+  });
+};
 
-exports.getSingleTour = (req, res) => {};
+exports.getSingleTour = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "tour get successfully",
+    data: {},
+  });
+};
 
-exports.createNewTour = (req, res) => {};
+exports.createNewTour = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "tour created successfully",
+    data: {},
+  });
+};
 
-exports.updateTour = (req, res) => {};
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "tour update successfully",
+    data: {},
+  });
+};
 
-exports.deleteTour = (req, res) => {};
+exports.deleteTour = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "tour deleted successfully",
+    data: {},
+  });
+};
--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -1,9 +1,39 @@
-exports.getAllUsers = (req, res) => {};
+exports.getAllUsers = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "users get successfully",
+    data: {},
+  });
+};
 
-exports.createNewUser = (req, res) => {};
+exports.getSingleUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "user get successfully",
+    data: {},
+  });
+};
 
-exports.getSingleUser = (req, res) => {};
+exports.createNewUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "user created successfully",
+    data: {},
+  });
+};
 
-exports.updateUser = (req, res) => {};
+exports.updateUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "user update successfully",
+    data: {},
+  });
+};
 
-exports.deleteUser = (req, res) => {};
+exports.deleteUser = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "user deleted successfully",
+    data: {},
+  });
+};
